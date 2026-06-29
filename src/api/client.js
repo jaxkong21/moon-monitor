acebe305fd3f4704de3d943ae45845a7f78355af
jaxkong21 <jaxtruong@gmail.com>
2026-06-29 10:18:29 -0700

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API = 'http://127.0.0.1:5000'
+const API = 'https://lethal-api-production.up.railway.app'
 
 export async function getMoons() {
     const response = await fetch(`${API}/moons`)
